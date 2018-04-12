28143153f0c22bbe866aed79d7b70b2dcec572ee
Emre Sokullu <esokullu@gmail.com>
2018-04-12 10:57:55 -0700

    Create recompiler.sh

---
+++ recompiler.sh
@@ -0,0 +1,45 @@
+# two variables
+# compilation dir
+# pho-cli binary
+
+mkdir ~/Downloads/recompile
+cd ~/Downloads/recompile
+
+git clone https://github.com/pho-recipes/Basic
+git clone https://github.com/pho-recipes/Twitter-simple
+git clone https://github.com/pho-recipes/Twitter-full
+git clone https://github.com/pho-recipes/Facebook
+git clone https://github.com/pho-recipes/Web
+
+rm -fr Basic/.compiled/
+rm -fr Twitter-simple/.compiled/
+rm -fr Twitter-full/.compiled/
+rm -fr Facebook/.compiled/
+rm -fr Web/.compiled/
+
+~/Code/pho-cli/bin/pho.php build ./Basic/ ./Basic/.compiled pgql
+~/Code/pho-cli/bin/pho.php build ./Twitter-simple/ ./Twitter-simple/.compiled pgql
+~/Code/pho-cli/bin/pho.php build ./Twitter-full/ ./Twitter-full/.compiled pgql
+~/Code/pho-cli/bin/pho.php build ./Facebook/ ./Facebook/.compiled pgql
+~/Code/pho-cli/bin/pho.php build ./Web/ ./Web/.compiled pgql
+
+cd Basic
+git commit -am "recompiled"
+git push
+
+cd ../Twitter-simple
+git commit -am "recompiled"
+git push
+
+cd ../Twitter-full
+git commit -am "recompiled"
+git push
+
+cd ../Facebook
+git commit -am "recompiled"
+git push
+
+cd ../Web
+git commit -am "recompiled"
+git push
+
