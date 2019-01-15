a36b0ec10909ee46dbe86f64eb0a78e2192f8a19
Emre Sokullu <esokullu@gmail.com>
2019-01-14 23:16:56 -0800

    added Network

--- recompiler.sh
+++ recompiler.sh
@@ -10,18 +10,21 @@ git clone https://github.com/pho-recipes/Twitter-simple
 git clone https://github.com/pho-recipes/Twitter-full
 git clone https://github.com/pho-recipes/Facebook
 git clone https://github.com/pho-recipes/Web
+git clone https://github.com/pho-recipes/Network
 
 rm -fr Basic/.compiled/
 rm -fr Twitter-simple/.compiled/
 rm -fr Twitter-full/.compiled/
 rm -fr Facebook/.compiled/
 rm -fr Web/.compiled/
+rm -fr Network/.compiled/
 
 ~/Code/pho-cli/bin/pho.php build ./Basic/ ./Basic/.compiled pgql
 ~/Code/pho-cli/bin/pho.php build ./Twitter-simple/ ./Twitter-simple/.compiled pgql
 ~/Code/pho-cli/bin/pho.php build ./Twitter-full/ ./Twitter-full/.compiled pgql
 ~/Code/pho-cli/bin/pho.php build ./Facebook/ ./Facebook/.compiled pgql
 ~/Code/pho-cli/bin/pho.php build ./Web/ ./Web/.compiled pgql
+~/Code/pho-cli/bin/pho.php build ./Network/ ./Network/.compiled pgql
 
 cd Basic
 git add .compiled && git commit -am "recompiled"
@@ -40,6 +43,10 @@ git add .compiled && git commit -am "recompiled"
 git push
 
 cd ../Web
+git add .compiled && git commit -am "recompiled"
+git push
+
+cd ../Network
 git add .compiled && git commit -am "recompiled"
 git push
 
